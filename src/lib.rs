b47c16fa6e2b8284cd56d5ba21abd1d208e8bc75
Tiziano Santoro <tiziano88@gmail.com>
2017-04-28 23:43:11 +0200

    Module doc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+//! # Scroll pHAT HD
+//!
+//! Sample usage:
+//!
+//! ```
+//! let mut display = I2CDisplay::new(1);
+//! let mut scroller = Scroller::new(&mut display);
+//! scroller.set_text("ABC");
+//! scroller.show();
+//! ```
+
 mod font;
 mod shared;
 
