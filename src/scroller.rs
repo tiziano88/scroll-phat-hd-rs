74e239f1c86c3b7b366906776dbb21bb05f2c677
Tiziano Santoro <tiziano88@gmail.com>
2017-04-28 21:20:48 +0200

    Doc diagram.

--- src/scroller.rs
+++ src/scroller.rs
@@ -3,26 +3,38 @@ use font::*;
 use shared::*;
 
 /// A virtual scrollable buffer with a scoll offset defining a visible window on the buffer itself.
+/// It is composed by a horizontally growable virtual buffer, and an offset that defines what
+/// portion of the buffer is actually visible.
+///
+/// ```text
+/// ┌─────────────virtual buffer───────────┐
+/// ┌───────╔═════════════════╗────────────┐
+/// │▓▓▓▓▓▓▓║░░░░░░░░░░░░░░░░░║▓▓▓▓▓▓▓▓▓▓▓▓│
+/// │▓▓▓▓▓▓▓║░░░░░░░░░░░░░░░░░║▓▓▓▓▓▓▓▓▓▓▓▓│
+/// └───────╚═════════════════╝────────────┘
+/// └offset─┴─visible buffer──┘
+/// ```
 pub struct Scroller<'a> {
-    scroll: usize,
-    buffer: Vec<Column>,
+    virtual_buffer: Vec<Column>,
+    scroll_offset_x: usize,
     display: &'a mut Display,
 }
 
 impl<'a> Scroller<'a> {
     pub fn new(display: &'a mut Display) -> Scroller {
         Scroller {
-            scroll: 0,
-            buffer: vec![],
+            virtual_buffer: vec![],
+            scroll_offset_x: 0,
             display: display,
         }
     }
 
-    /// Moves the buffer one pixel to the right.
+    /// Moves the buffer one pixel to the right. When the window reaches the end of the virtual
+    /// buffer, it starts again from the beginning.
     pub fn scroll(&mut self) {
-        self.scroll += 1;
-        if self.scroll >= self.buffer.len() {
-            self.scroll = 0;
+        self.scroll_offset_x += 1;
+        if self.scroll_offset_x >= self.virtual_buffer.len() {
+            self.scroll_offset_x = 0;
         }
     }
 
@@ -31,12 +43,12 @@ impl<'a> Scroller<'a> {
         if y >= DISPLAY_HEIGHT {
             return;
         }
-        if x >= self.buffer.len() {
-            for _ in self.buffer.len()..(x + 1) {
-                self.buffer.push(EMPTY_COLUMN);
+        if x >= self.virtual_buffer.len() {
+            for _ in self.virtual_buffer.len()..(x + 1) {
+                self.virtual_buffer.push(EMPTY_COLUMN);
             }
         }
-        self.buffer[x][y] = value;
+        self.virtual_buffer[x][y] = value;
     }
 
     /// Sets the text of the buffer to the specified value.
@@ -46,22 +58,30 @@ impl<'a> Scroller<'a> {
         for c in text.chars() {
             if let Some(glyph) = font.get(&c) {
                 for c in glyph {
-                    self.buffer.push(*c);
+                    self.virtual_buffer.push(*c);
                 }
-                self.buffer.push(EMPTY_COLUMN);
+                self.virtual_buffer.push(EMPTY_COLUMN);
             }
         }
     }
 
-    /// Sends the buffer to the output display.
+    fn visible_buffer(&self) -> Vec<Column> {
+        self.virtual_buffer
+            .iter()
+            .skip(self.scroll_offset_x)
+            .take(DISPLAY_WIDTH)
+            .cloned()
+            .collect()
+    }
+
+    /// Sends the visible buffer to the output display.
     pub fn show(&mut self) {
-        let buffer: Vec<Column> =
-            self.buffer.iter().skip(self.scroll).take(DISPLAY_WIDTH).cloned().collect();
-        self.display.show(&buffer);
+        let visible_buffer = self.visible_buffer();
+        self.display.show(&visible_buffer);
     }
 
     /// Clears the buffer.
     pub fn clear(&mut self) {
-        self.buffer = vec![];
+        self.virtual_buffer = vec![];
     }
 }
