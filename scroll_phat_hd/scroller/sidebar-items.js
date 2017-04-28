13935e6341ebf9b910215488355fb548c1840562
Tiziano Santoro <tiziano88@gmail.com>
2017-04-28 21:22:48 +0200

    update doc

--- scroll_phat_hd/scroller/sidebar-items.js
+++ scroll_phat_hd/scroller/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Scroller","A virtual scrollable buffer with a scoll offset defining a visible window on the buffer itself."]]});
+initSidebarItems({"struct":[["Scroller","A virtual scrollable buffer with a scoll offset defining a visible window on the buffer itself. It is composed by a horizontally growable virtual buffer, and an offset that defines what portion of the buffer is actually visible."]]});
